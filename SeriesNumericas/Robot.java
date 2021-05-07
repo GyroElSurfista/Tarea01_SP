7a19dda42fc6922acd8ac3d9db5710e8943c2455
GyroElSurfista <81333058+GyroElSurfista@users.noreply.github.com>
2021-05-07 17:18:56 -0400

    Add files via upload

---
+++ SeriesNumericas/Robot.java
@@ -0,0 +1,48 @@
+  public class Robot
+  {
+    public void ejercicio1(int n){
+        int num = 1;
+        int contador = 0;
+        if(n == 1){
+           System.out.print(1);
+        }else if(n > 0){
+           System.out.print(1 + " ");
+               for(int i = 2 ; i <= n ; i++){
+                contador++;
+               if(contador == 4){
+                  contador = 2;
+                  num = num + contador;
+                  System.out.print(num + " ");
+                  contador = 0;
+               }else{
+                  num = num + contador;
+                  System.out.print(num + " ");
+               
+               }
+           }
+        }
+    }
+    
+    public void ejercicio2(int n){
+        int num1 = 1;
+        int num2 = 1;
+        int num3 = 1;
+        int num4 = 0;
+        if(n <= 3 && n > 0){
+            for(int i = 1; i <= n; i++){
+                System.out.print(1 + " ");
+            }
+        }else if(n > 0){
+            for(int i = 1; i <= 3; i++){
+                System.out.print(1 + " ");
+            }
+            for(int i = 4; i <= n; i++){
+                num4 = num1 + num2;
+                System.out.print(num4 + " ");
+                num1 = num2;
+                num2 = num3;
+                num3 = num4;           
+            }
+        }
+    }
+}
